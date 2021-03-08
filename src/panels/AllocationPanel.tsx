7614c87a3ab3e337a6a0ac396900ab22f644773f
noobless <nuzulzen@gmail,com>
2021-03-08 17:10:44 +0700

    add markup component

--- src/panels/AllocationPanel.tsx
+++ src/panels/AllocationPanel.tsx
@@ -11,6 +11,7 @@ import {
 import { getFundAllocation } from "../services/dataService";
 import { Allocation } from "../data/models";
 
+
 export default function AllocationPanel() {
   const [data, setData] = React.useState<Allocation[]>();
   React.useEffect(() => {
@@ -20,6 +21,18 @@ export default function AllocationPanel() {
   }, []);
 
   return (
-    <h2>Allocation Panel</h2>
+   <Chart>
+     <ChartTitle text={"Asset Allocation "}></ChartTitle>
+     <ChartSeries>
+       <ChartSeriesItem type="donut" data={data}>
+         <ChartSeriesLabels content={e => `${e.value}%`} background="none" color="fff">
+         </ChartSeriesLabels>
+       </ChartSeriesItem>
+     </ChartSeries>
+    <ChartLegend position={"bottom"} visible={true} />
+     <ChartTooltip render={(e: any) => (
+       <div>{e.point ?  e.point.category :  ""}</div>
+     ) }/>
+   </Chart>
   )
 }
